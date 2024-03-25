15217d938aa182ea1fd697eb4e8076b6a52dd975
peterskov1997 <peterskov1997@live.dk>
2024-03-25 01:49:49 +0100

    Refactored Address and added some test

--- EmergencyResponse/EmergencyResponse/Model/Address.cs
+++ EmergencyResponse/EmergencyResponse/Model/Address.cs
@@ -2,13 +2,34 @@
 {
     public class Address
     {
-        public string Id { get; set; }
-        public string StreetName { get; set; }
-        public string HouseNumber { get; set; }
-        public int? Floor { get; set; }
-        public string? Door { get; set; }
-        public string PostalCode { get; set; }
-        public string PostalCodeName { get; set; }
-        public string? AddressId { get; set; }
+        public string Id { get; private set; }
+        public string StreetName { get; private set; }
+        public string HouseNumber { get; private set; }
+        public int? Floor { get; private set; }
+        public string? Door { get; private set; }
+        public string PostalCode { get; private set; }
+        public string PostalCodeName { get; private set; }
+        public string? AddressId { get; private set; }
+
+        public Address(string id, string streetName, string houseNumber, int? floor, string door, string postalCode, string postalCodeName)
+        {
+            Id = id ?? throw new ArgumentNullException(nameof(id), "Id cannot be null.");
+            StreetName = streetName ?? throw new ArgumentNullException(nameof(streetName), "StreetName cannot be null.");
+            HouseNumber = houseNumber ?? throw new ArgumentNullException(nameof(houseNumber), "HouseNumber cannot be null.");
+            Floor = floor; // Nullable, no check needed
+            Door = door; // Nullable, no check needed
+            PostalCode = postalCode ?? throw new ArgumentNullException(nameof(postalCode), "PostalCode cannot be null.");
+            PostalCodeName = postalCodeName ?? throw new ArgumentNullException(nameof(postalCodeName), "PostalCodeName cannot be null.");
+        }
+
+        public void SetAddressId(string addressId)
+        {
+            if (AddressId != null)
+            {
+                throw new InvalidOperationException("AddressId can only be set once.");
+            }
+
+            AddressId = addressId ?? throw new ArgumentNullException(nameof(addressId), "AddressId cannot be null.");
+        }
     }
 }
--- EmergencyResponse/EmergencyResponseTest/UnitTest1.cs
+++ EmergencyResponse/EmergencyResponseTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using EmergencyResponse.Model;
+using FluentAssertions;
 using Xunit;
 
 namespace EmergencyResponseTest
@@ -9,7 +10,7 @@ namespace EmergencyResponseTest
        // [TestClass]
         public class APIValidatorTests
         {
-           // private APIValidator _validator;
+            // private APIValidator _validator;
 
             /*[SetUp]
             public void Setup()
@@ -19,38 +20,21 @@ namespace EmergencyResponseTest
             }*/
 
             [Theory]
-            [InlineData("0a31asdac-2mnsb-3218-e042-0003ba293131", "Oak Avenue", "1011", null, "D", "13579", "City D", "addr4")]
-            public void Create_Address(string id, string streetName, string houseNumber, int? floor, string door,
-                string postalCode, string postalCodeName, string addressId)
+            [InlineData("0a31asdac-2mnsb-3218-e042-0003ba293131", "Oak Avenue", "1011", null, "D", "13579", "City D")]
+            public void AddressProperties_ShouldMatchInputValues(string id, string streetName, string houseNumber, int? floor, string door,
+                string postalCode, string postalCodeName)
             {
-                //Arrange
-                Address result = new Address
-                {
-                    Id = "0a31asdac-2mnsb-3218-e042-0003ba293131",
-                    StreetName = "Oak Avenue",
-                    HouseNumber = "1011",
-                    Floor = null,
-                    Door = "D",
-                    PostalCode = "13579",
-                    PostalCodeName = "City D"
-                };
-
-                //Act
-                Address expectedResult = new Address
-                {
-                    Id = id,
-                    StreetName = streetName,
-                    HouseNumber = houseNumber,
-                    Floor = floor,
-                    Door = door,
-                    PostalCode = postalCode,
-                    PostalCodeName = postalCodeName,
-                    AddressId = addressId
-                };
-
+                // Act
+                var address = new Address(id, streetName, houseNumber, floor, door, postalCode, postalCodeName);
 
                 // Assert
-                expectedResult.Equals(result);
+                Assert.Equal(id, address.Id);
+                Assert.Equal(streetName, address.StreetName);
+                Assert.Equal(houseNumber, address.HouseNumber);
+                Assert.Equal(floor, address.Floor);
+                Assert.Equal(door, address.Door);
+                Assert.Equal(postalCode, address.PostalCode);
+                Assert.Equal(postalCodeName, address.PostalCodeName);
             }
 
 
@@ -58,102 +42,133 @@ namespace EmergencyResponseTest
             {
                 yield return new object[]
                 {
-                null, null, null, null, null, null, null, null
+                null, null, null, null, null, null, null
                 };
             }
 
             [Theory, MemberData(nameof(GetAddressData))]
-            public void MemberDataAddition_ShouldCalculateCorrectly(string id, string streetName, string houseNumber, int? floor, string door,
-                string postalCode, string postalCodeName, string addressId)
+            public void AddressConstructor_WithNullValuesForNonNullableProperties_ShouldThrowArgumentNullException(string id, string streetName, string houseNumber, int? floor, string door,
+                string postalCode, string postalCodeName)
             {
-                //Act
-                Action act = () => {
-                    Address expectedResult = new Address
-                    {
-                        Id = id,
-                        StreetName = streetName,
-                        HouseNumber = houseNumber,
-                        Floor = floor,
-                        Door = door,
-                        PostalCode = postalCode,
-                        PostalCodeName = postalCodeName,
-                        AddressId = addressId
-                    };
-                };
-
-                var exception = Record.Exception(act);
-
+                // Act
+                Action action = () => new Address(id, streetName, houseNumber, floor, door, postalCode, postalCodeName);
 
                 // Assert
-                Assert.Throws<NullReferenceException>(act);
+                Assert.Throws<ArgumentNullException>(action);
             }
-/*
 
-            [TestMethod]
-            public void ValidateAddress_ShouldReturnTrue_ForValidAddress()
+            [Fact]
+            public void SetAddressId_WhenCalledFirstTime_SetsTheAddressId()
             {
                 // Arrange
-                var validAddress = new AddressDTO("ValidStreet", "ValidCity", "1234");
+                var address = new Address("id", "streetName", "houseNumber", null, "door", "postalCode", "postalCodeName");
+                var addressId = "newAddressId";
 
                 // Act
-                var result = _validator.ValidateAddress(validAddress);
+                Action act = () => address.SetAddressId(addressId);
 
                 // Assert
-                Assert.IsTrue(result, "Expected validation to pass for a valid address.");
+                act.Should().NotThrow();
+                address.AddressId.Should().Be(addressId);
             }
 
-            [TestMethod]
-            public void ValidateAddress_ShouldReturnFalse_ForInvalidZipCode()
+            [Fact]
+            public void SetAddressId_ForTheSecondTime_ThrowsInvalidOperationException()
             {
                 // Arrange
-                var addressWithInvalidZip = new AddressDTO("ValidStreet", "ValidCity", "InvalidZip");
+                var address = new Address("id", "streetName", "houseNumber", null, "door", "postalCode", "postalCodeName");
+                address.SetAddressId("firstUniqueId"); // First time setting AddressId
 
                 // Act
-                var result = _validator.ValidateAddress(addressWithInvalidZip);
+                Action act = () => address.SetAddressId("secondUniqueId"); // Attempting to set again
 
                 // Assert
-                Assert.IsFalse(result, "Expected validation to fail for an invalid zip code.");
+                act.Should().Throw<InvalidOperationException>().WithMessage("AddressId can only be set once.");
             }
 
-            [TestMethod]
-            public void ValidateAddress_ShouldReturnFalse_ForEmptyStreet()
+            [Theory]
+            [InlineData(null, typeof(ArgumentNullException), "AddressId cannot be null. (Parameter 'addressId')")]
+            public void SetAddressId_FailureScenarios_ThrowsExpectedException(string AddressId, Type expectedException, string expectedMessage)
             {
                 // Arrange
-                var addressWithEmptyStreet = new AddressDTO("", "ValidCity", "1234");
+                var address = new Address("id", "streetName", "houseNumber", null, "door", "postalCode", "postalCodeName");
 
                 // Act
-                var result = _validator.ValidateAddress(addressWithEmptyStreet);
+                Action act = () => address.SetAddressId(AddressId);
 
                 // Assert
-                Assert.IsFalse(result, "Expected validation to fail for an empty street.");
+                act.Should().Throw<Exception>().Where(e => e.GetType() == expectedException && e.Message.Contains(expectedMessage));
             }
 
-            [TestMethod]
-            public void ValidateAddress_ShouldReturnFalse_ForEmptyCity()
-            {
-                // Arrange
-                var addressWithEmptyCity = new AddressDTO("ValidStreet", "", "1234");
 
-                // Act
-                var result = _validator.ValidateAddress(addressWithEmptyCity);
 
-                // Assert
-                Assert.IsFalse(result, "Expected validation to fail for an empty city.");
-            }
+            /*
 
-            [TestMethod]
-            public void ValidateAddress_ShouldReturnFalse_ForEmptyZipCode()
-            {
-                // Arrange
-                var addressWithEmptyZip = new AddressDTO("ValidStreet", "ValidCity", "");
+                        [TestMethod]
+                        public void ValidateAddress_ShouldReturnTrue_ForValidAddress()
+                        {
+                            // Arrange
+                            var validAddress = new AddressDTO("ValidStreet", "ValidCity", "1234");
 
-                // Act
-                var result = _validator.ValidateAddress(addressWithEmptyZip);
+                            // Act
+                            var result = _validator.ValidateAddress(validAddress);
 
-                // Assert
-                Assert.IsFalse(result, "Expected validation to fail for an empty zip code.");
-            }
-*/
+                            // Assert
+                            Assert.IsTrue(result, "Expected validation to pass for a valid address.");
+                        }
+
+                        [TestMethod]
+                        public void ValidateAddress_ShouldReturnFalse_ForInvalidZipCode()
+                        {
+                            // Arrange
+                            var addressWithInvalidZip = new AddressDTO("ValidStreet", "ValidCity", "InvalidZip");
+
+                            // Act
+                            var result = _validator.ValidateAddress(addressWithInvalidZip);
+
+                            // Assert
+                            Assert.IsFalse(result, "Expected validation to fail for an invalid zip code.");
+                        }
+
+                        [TestMethod]
+                        public void ValidateAddress_ShouldReturnFalse_ForEmptyStreet()
+                        {
+                            // Arrange
+                            var addressWithEmptyStreet = new AddressDTO("", "ValidCity", "1234");
+
+                            // Act
+                            var result = _validator.ValidateAddress(addressWithEmptyStreet);
+
+                            // Assert
+                            Assert.IsFalse(result, "Expected validation to fail for an empty street.");
+                        }
+
+                        [TestMethod]
+                        public void ValidateAddress_ShouldReturnFalse_ForEmptyCity()
+                        {
+                            // Arrange
+                            var addressWithEmptyCity = new AddressDTO("ValidStreet", "", "1234");
+
+                            // Act
+                            var result = _validator.ValidateAddress(addressWithEmptyCity);
+
+                            // Assert
+                            Assert.IsFalse(result, "Expected validation to fail for an empty city.");
+                        }
+
+                        [TestMethod]
+                        public void ValidateAddress_ShouldReturnFalse_ForEmptyZipCode()
+                        {
+                            // Arrange
+                            var addressWithEmptyZip = new AddressDTO("ValidStreet", "ValidCity", "");
+
+                            // Act
+                            var result = _validator.ValidateAddress(addressWithEmptyZip);
+
+                            // Assert
+                            Assert.IsFalse(result, "Expected validation to fail for an empty zip code.");
+                        }
+            */
         }
     }
 }
