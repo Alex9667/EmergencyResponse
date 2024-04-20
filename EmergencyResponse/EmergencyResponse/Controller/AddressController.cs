bef91703bbd5136445ef296c0c6764dee201579b
Alexander <blablabla2013@live.dk>
2024-04-21 01:10:52 +0200

    Added method to AddressController to get the bfe from an address

--- EmergencyResponse/EmergencyResponse/Controller/AddressController.cs
+++ EmergencyResponse/EmergencyResponse/Controller/AddressController.cs
@@ -1,13 +1,79 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using EmergencyResponse.Model;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 
 namespace EmergencyResponse.Controller
 {
     public class AddressController : ControllerBase
     {
+        private readonly HttpClient _httpClient;
+
+        public AddressController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public Address GetAddress()
         {
             throw new NotImplementedException();
         }
+
+        public async Task<int> GetAddressBFE(string address)
+        {
+            string bfeStepOne = await _httpClient.GetStringAsync("https://api.dataforsyningen.dk/adresser?q=" + address + "&struktur=mini");
+            string id;
+
+            // Parse the JSON string into a JsonDocument
+            using (JsonDocument doc = JsonDocument.Parse(bfeStepOne))
+            {
+                // Gets the root element of the JSON text
+                JsonElement root = doc.RootElement;
+
+                // Gets the first element of the root
+                JsonElement firstElement = root[0];
+
+                // Get the id property
+                id = firstElement.GetProperty("id").GetString();
+            }
+
+            string bfeStepTwo = await _httpClient.GetStringAsync("https://services.datafordeler.dk/DAR/DAR/2.0.0/rest/adresse?id=" + id + "&username=QRUSLIHSDE&password=SOFTWAREKval!tet2024");
+            string landPiece;
+
+            using (JsonDocument doc = JsonDocument.Parse(bfeStepTwo))
+            {
+                // Gets the root element of the JSON text
+                JsonElement root = doc.RootElement[0];
+
+                // Gets the land piece
+                landPiece= root.GetProperty("husnummer").GetProperty("jordstykke").GetString();
+            }
+
+            string bfeStepThree = await _httpClient.GetStringAsync("https://services.datafordeler.dk//BBR/BBRPublic/1/rest/grund?jordstykke=" + landPiece + "&&username=QRUSLIHSDE&password=SOFTWAREKval!tet2024");
+            int bfe;
+
+            using (JsonDocument doc = JsonDocument.Parse(bfeStepThree))
+            {
+                // Gets the root element of the JSON text
+                JsonElement root = doc.RootElement[0];
+
+                // Gets the land piece
+                bfe = root.GetProperty("bestemtFastEjendom").GetProperty("bfeNummer").GetInt32();  
+            }
+            return bfe;
+        }
     }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class AddressId
+    {
+        public string id { get; set; }
+        // Add other properties as needed
+    }
+
 }
--- EmergencyResponse/EmergencyResponseTest/AddressControllerTest.cs
+++ EmergencyResponse/EmergencyResponseTest/AddressControllerTest.cs
@@ -5,23 +5,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EmergencyResponse.Controller;
+using System.Net.Http;
+
 
 namespace EmergencyResponseTest
 {
     public class AddressControllerTest
     {
-        private AddressController _addressController;
-        public AddressControllerTest(AddressController addressController) 
+        private readonly AddressController _addressController;
+
+        public AddressControllerTest()
         {
-            _addressController = addressController;
+            HttpClient httpClient = new HttpClient(); 
+            _addressController = new AddressController(httpClient);
         }
 
         [Fact]
         public void AddressController_GetAddress_ShouldReturnData()
         {
             Address address = _addressController.GetAddress();
-
             address.Should().NotBeNull();   
+        }
+
+        [Theory]
+        [InlineData("Nygade 74 7430 Ikast")]
+        public void AddressController_GetAddressBFE_ShouldReturnString(string address)
+        {
+            Task<int> bfe = _addressController.GetAddressBFE(address);
+            bfe.ToString().Should().NotBeNullOrEmpty();
         }
     }
 }
