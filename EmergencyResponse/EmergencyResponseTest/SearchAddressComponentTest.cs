fc7a65ffc68d490c5f78fda8afda2ddb3df325c3
Alex9667 <blablabla2013@live.dk>
2024-05-17 10:17:43 +0200

    Fixed mocks For tests

--- EmergencyResponse/EmergencyResponseTest/SearchAddressComponentTest.cs
+++ EmergencyResponse/EmergencyResponseTest/SearchAddressComponentTest.cs
@@ -8,18 +8,51 @@ using EmergencyResponse.Pages;
 using EmergencyResponse.ExternalServices.Interfaces;
 using Moq;
 using EmergencyResponse.DTO;
-
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EmergencyResponseTest
 {
     public class SearchAddressComponentTest
     {
+        private readonly TestContext _ctx;
+        private readonly Mock<IDataforsyningService> _mockDataforsyningService;
+        private readonly Mock<IDatafordelerenService> _mockDatafordelerenService;
+
+        public SearchAddressComponentTest()
+        {
+            _ctx = new TestContext();
+
+            // Create mock services
+            _mockDataforsyningService = new Mock<IDataforsyningService>();
+            _mockDatafordelerenService = new Mock<IDatafordelerenService>();
+
+            // Register the mock services in the test context
+            _ctx.Services.AddSingleton<IDataforsyningService>(_mockDataforsyningService.Object);
+            _ctx.Services.AddSingleton<IDatafordelerenService>(_mockDatafordelerenService.Object);
+        }
+
+
+
         [Fact]
         public async void SearchAddress_ShouldReturnListOfAddresses()
         {
-            // Arrange
-            using var ctx = new TestContext();
-            var cut = ctx.RenderComponent<SearchAddressComponent>();
+            // Setup the mock to return a list of addresses
+            var addresses = new List<AddressDTO>
+            {
+                new AddressDTO
+                {
+                    StreetName = "Fjellerupvej",
+                    HouseNumber = "7",
+                    PostalCode = "5463",
+                    PostalCodeName = "SomeCity",
+                    Floor = "1",
+                    Door = "A",
+                    BFE = 12345
+                }
+            };
+            _mockDataforsyningService.Setup(service => service.SearchAddress(It.IsAny<AddressDTO>())).ReturnsAsync(addresses);
+
+            var cut = _ctx.RenderComponent<SearchAddressComponent>();
 
             // Act - simulate user input and button click
             var input1 = cut.Find("input[id='street']");
@@ -36,9 +69,6 @@ namespace EmergencyResponseTest
 
             var submitButton = cut.Find("button[id='submit']");
             submitButton.Click(); // Simulate button click
-
-            //Awaiting because the submitButton.Click event calls an async method that won't finish before the test continues.
-            await Task.Delay(1000);
 
             // Assert
             var componentInstance = cut.Instance;
